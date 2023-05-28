104b4a4d2bb7870a0ef1491dcc1429e9a0a457fd
RanaHamzaHameed <hhameed032444@gmail.com>
2023-05-28 20:17:09 +0500

    SignUp Screen

--- ReactNative/pages/SignUpScreen.js
+++ ReactNative/pages/SignUpScreen.js
@@ -8,8 +8,78 @@ import Large_Button from '../components/Large_Button';
 import Normal_Text from '../components/Normal_Text';
 import SubHeading from '../components/SubHeading';
 import Circle from '../components/Circle';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+
+// Firebase configuration object
+const firebaseConfig = {
+  apiKey: "AIzaSyB874SwK2FLGOf3PTtDbmAmUEDwCmejqH4",
+  authDomain: "bookdiscoveryapp.firebaseapp.com",
+  databaseURL: "https://bookdiscoveryapp-default-rtdb.firebaseio.com",
+  projectId: "bookdiscoveryapp",
+  storageBucket: "bookdiscoveryapp.appspot.com",
+  messagingSenderId: "224686141258",
+  appId: "1:224686141258:web:4228c07458829be9f25bb3",
+  measurementId: "G-0CTY4WGY2C"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 const defaultProfilePic = require('../assets/defaultpp.png');
+
+const SignUpScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [registrationStatus, setRegistrationStatus] = useState('');
+
+  const navigation = useNavigation();
+
+  const handleSignUpPress = () => {
+    if (password !== confirmPassword) {
+      setRegistrationStatus('passwordMismatch');
+      return;
+    }
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const userUid = userCredential.user.uid;
+        const usersRef = firebase.database().ref('User');
+        const newUserRef = usersRef.child(userUid);
+
+        newUserRef
+          .set({
+            username: name,
+            email: email,
+            password: password,
+            profile_pic: defaultProfilePic,
+          })
+          .then(() => {
+            setRegistrationStatus('success');
+            console.log('User registered:', userCredential.user);
+            navigation.navigate('Login');
+          })
+          .catch((error) => {
+            setRegistrationStatus('errorWritingData');
+            console.log('Error writing user data:', error);
+          });
+      })
+      .catch((error) => {
+        if (error.code === 'auth/email-already-in-use') {
+          setRegistrationStatus('emailAlreadyRegistered');
+          console.log('Email already registered');
+        } else {
+          setRegistrationStatus('registrationError');
+          console.log('Registration error:', error.message);
+        }
+      });
+  };
 
   const handleLoginPress = () => {
     navigation.navigate('Login');
@@ -38,15 +108,15 @@ const defaultProfilePic = require('../assets/defaultpp.png');
         <Logo topPosition={74} />
 
         <Form_Label text="Username" topPosition={252} leftPosition={92} />
-        <Large_Form value={name} onChangeText={} topPosition={278} />
+        <Large_Form value={name} onChangeText={setName} topPosition={278} />
 
         <Form_Label text="Email" topPosition={357} leftPosition={92} />
-        <Large_Form value={email} onChangeText={} topPosition={383} />
+        <Large_Form value={email} onChangeText={setEmail} topPosition={383} />
 
         <Form_Label text="Password" topPosition={461} leftPosition={92} />
         <Large_Form
           value={password}
-          onChangeText={}
+          onChangeText={setPassword}
           topPosition={487}
           secureTextEntry
         />
@@ -54,12 +124,12 @@ const defaultProfilePic = require('../assets/defaultpp.png');
         <Form_Label text="Confirm Password" topPosition={564} leftPosition={92} />
         <Large_Form
           value={confirmPassword}
-          onChangeText={}
+          onChangeText={setConfirmPassword}
           topPosition={590}
           secureTextEntry
         />
 
-        <Large_Button title="SignUp" topPosition={674} onPress={} />
+        <Large_Button title="SignUp" topPosition={674} onPress={handleSignUpPress} />
 
         <Normal_Text text="Already Registered? " topPosition={770} leftPosition={102} absolutePosition={true}/>
         <TouchableOpacity
@@ -78,6 +148,19 @@ const defaultProfilePic = require('../assets/defaultpp.png');
       </View>
     </ScrollView>
   );
+};
 
+const styles = {
+  successMessage: {
+    color: 'green',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  errorMessage: {
+    color: 'red',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+};
 
 export default SignUpScreen;
