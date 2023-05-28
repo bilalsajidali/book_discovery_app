09ae7bbfa6cf044fbce0a62e96cbdc177dae5c74
RanaHamzaHameed <hhameed032444@gmail.com>
2023-05-28 17:39:19 +0500

    auth

--- ReactNative/pages/LoginScreen.js
+++ ReactNative/pages/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, ScrollView, TouchableOpacity, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import Large_Form from '../components/Large_Form';
 import Form_Label from '../components/Form_Label'
 import Normal_Text from '../components/Normal_Text';
@@ -7,54 +8,95 @@ import Logo from '../components/Logo';
 import Large_Button from '../components/Large_Button';
 import SubHeading from '../components/SubHeading';
 import Circle from '../components/Circle';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
 
-const LoginScreen = ({ navigation }) => {
-  const [name, setName] = useState('');
+// Firebase configuration object
+const firebaseConfig = {
+    apiKey: "AIzaSyB874SwK2FLGOf3PTtDbmAmUEDwCmejqH4",
+    authDomain: "bookdiscoveryapp.firebaseapp.com",
+    databaseURL: "https://bookdiscoveryapp-default-rtdb.firebaseio.com",
+    projectId: "bookdiscoveryapp",
+    storageBucket: "bookdiscoveryapp.appspot.com",
+    messagingSenderId: "224686141258",
+    appId: "1:224686141258:web:4228c07458829be9f25bb3",
+    measurementId: "G-0CTY4WGY2C"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigation = useNavigation();
+
+  const handleSignUpPress = () => {
+    navigation.navigate('SignUp');
+  };
+
+  const handleLoginPress = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        console.log('User logged in:', userCredential.user);
+        navigation.navigate('Create');
+      })
+      .catch((error) => {
+        setErrorMessage('Incorrect email or password');
+      });
+  };
 
   return (
-    <View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View>
         <Logo topPosition={105} />
 
         <Normal_Text
-            text="Your Personalized Literary Journey"
-            topPosition={295}
-            leftPosition={59}
+          text="Your Personalized Literary Journey"
+          topPosition={310}
+          leftPosition={59}
+          absolutePosition={true}
         />
+
+        {errorMessage ? (
+          <Text style={styles.errorMessage}>{errorMessage}</Text>
+        ) : null}
 
         <Form_Label text="Email" topPosition={404} leftPosition={92} />
-        <Large_Form value={name} onChangeText={setName} topPosition={430} />
+        <Large_Form value={email} onChangeText={setEmail} topPosition={430} />
 
         <Form_Label text="Password" topPosition={513} leftPosition={92} />
-        <Large_Form value={name} onChangeText={setName} topPosition={538} />
+        <Large_Form value={password} onChangeText={setPassword} topPosition={538} secureTextEntry />
 
-        <Large_Button title="Login" topPosition={621} />
+        <Large_Button title="Login" topPosition={621} onPress={handleLoginPress} />
 
-        <SubHeading
-            text="Forget Password?"
-            topPosition={699}
-            leftPosition={136}
-            color="#262A56"
-        />
+        <SubHeading text="Forget Password?" topPosition={699} leftPosition={136} color="#262A56" />
 
-        <Normal_Text
-            text="Don't have an account? "
-            topPosition={766}
-            leftPosition={67}
-        />
-        <SubHeading
-            text="SignUp"
-            topPosition={766}
-            leftPosition={238}
-            color="#262A56"
-        />
+        <Normal_Text text="Don't have an account? " topPosition={766} leftPosition={67} absolutePosition={true}/>
+        <TouchableOpacity style={{ position: 'absolute', top: 766, left: 282 }} onPress={handleSignUpPress}>
+          <SubHeading text="SignUp" color="#262A56" />
+        </TouchableOpacity>
 
         <Circle topPosition={-39} leftPosition={0}></Circle>
-        <Circle topPosition={395} leftPosition={270}> </Circle>
-        <Circle topPosition={651} leftPosition={-64}> </Circle>
-        <Circle topPosition={-39} leftPosition={0}> </Circle>
-
-    </View>
+        <Circle topPosition={395} leftPosition={270}></Circle>
+        <Circle topPosition={651} leftPosition={-64}></Circle>
+        <Circle topPosition={790} leftPosition={282}></Circle>
+      </View>
+    </ScrollView>
   );
+};
+
+const styles = {
+  errorMessage: {
+    color: 'red',
+    textAlign: 'center',
+    marginTop: 130,
+  },
 };
 
 export default LoginScreen;
